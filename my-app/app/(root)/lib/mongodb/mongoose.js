074808c932387ddb28752d61c7e51e6b6e5d9c50
yugsakariya12 <yugsakariya8105@gmail.com>
2025-06-24 21:23:39 +0530

    Log MONGODB_URI for debugging

--- my-app/app/(root)/lib/mongodb/mongoose.js
+++ my-app/app/(root)/lib/mongodb/mongoose.js
@@ -11,7 +11,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "VibeZone",
       useNewUrlParser: true,
       useUnifiedTopology: true,
