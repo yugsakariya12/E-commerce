884895bdc7a13060d89b2e040081e382e8807a05
yugsakariya12 <yugsakariya8105@gmail.com>
2025-06-24 19:13:02 +0530

    Moved lib folder, updated webhook route and package files

--- my-app/app/api/webhook/route.js
+++ my-app/app/api/webhook/route.js
@@ -1,6 +1,6 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
-import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
+import { createOrUpdateUser } from "@/app/lib/actions/user";
 
 export async function POST(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
